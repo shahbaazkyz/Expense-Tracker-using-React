351085e5c0a590a8a5031c5fcc7bb00692b70837
Nabia-Sheikh <nabiasheikh2222@gmail.com>
2021-09-01 17:31:31 +0500

    V1.1

--- src/components/History/HistoryItem.js
+++ src/components/History/HistoryItem.js
@@ -1,14 +1,25 @@
-function HistoryItem() {
+function HistoryItem(prop) {
+  const {historyData} = prop ;
   return (
     <ul id="list" className="list">
-      <li className="minus">
-        Cash <span>-$400</span>
-        <button class="delete-btn">x</button>
-      </li>
-      <li className="minus">
+      {
+        historyData.map((value , index) => {
+          return(
+        
+            <li className="minus">
+              {value.title}
+              <span>{value.amount}</span>
+              <button class="delete-btn">x</button>
+              </li>
+        
+          )
+        }
+        )
+      }
+      {/* <li className="minus">
         Rent <span>-$300</span>
         <button class="delete-btn">x</button>
-      </li>
+      </li> */}
     </ul>
   );
 }
